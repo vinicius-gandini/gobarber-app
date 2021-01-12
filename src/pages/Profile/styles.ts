8996a0634acb9be48e2aee8b6fe9a229034e97ca
vinicius-gandini <viniciusgandini.goncalves@gmail.com>
2021-01-11 22:36:38 -0300

    feat: :ok_hand: update user function

--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -4,7 +4,7 @@ import { Platform } from 'react-native';
 export const Container = styled.View`
   flex: 1;
   justify-content: center;
-  padding: 0 30px ${Platform.OS === 'android' ? 70 : 40}px;
+  padding: 0 30px ${Platform.OS === 'android' ? 100 : 40}px;
   position: relative;
 `;
 
@@ -26,7 +26,7 @@ export const UserAvatarButton = styled.TouchableOpacity`
 export const UserAvatar = styled.Image`
   width: 186px;
   height: 186px;
-  border-radius: 88px;
+  border-radius: 98px;
 
   align-self: center;
 `;
